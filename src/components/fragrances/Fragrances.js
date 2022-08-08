94b02b78b0613fc803a577585b53cb7833e89612
Adam <adam0tokyo@gmail.com>
2022-08-08 21:24:29 +0900

    feat: add Users and Fragrances components

---
+++ src/components/fragrances/Fragrances.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function Fragrances() {
+    //STATE
+    const [fragrances, setFragrances] = useState([]);
+
+    //USEEFFECT
+    useEffect(() => {
+        axios.get('/api/fragrances').then((res) => {
+            // console.log('AXIOS!:', res.data);
+            setFragrances(res.data);
+        });
+    });
+
+    //HELPER FUNCTIONS
+    // none currently
+
+    return (
+        <div className="Fragrances">
+            <h1>Fragrances</h1>
+            <ul>
+                {fragrances.map((fragrance) => {
+                    return (
+                        <>
+                            <li key={fragrance.id}>{fragrance.display_name}</li>
+                        </>
+                    );
+                })}
+                git
+            </ul>
+        </div>
+    );
+}
+
+export default Fragrances;
